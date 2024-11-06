c5ba200e5864acf583021372926dced7b7b240d4
Ciamac.Davoudi <ciamac.davoudil@interone.de>
2024-11-06 12:11:35 +0100

    Font added

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,23 @@
 import { Tabs } from "expo-router";
 import React from "react";
+import { StyleSheet } from "react-native";
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
-import Home from "@/components/Home";
+import Header from "@/components/Header";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
 
-  return <Home />;
+  return (
+    <div style={styles.html}>
+      <Header />
+    </div>
+  );
 }
+
+const styles = StyleSheet.create({
+  html: {
+    fontFamily: "Nasa21",
+  },
+});
