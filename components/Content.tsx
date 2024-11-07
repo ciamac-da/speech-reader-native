dd2ec177c3d20a696f6200c023598680e527d797
Ciamac.Davoudi <ciamac.davoudil@interone.de>
2024-11-07 14:05:24 +0100

    nvm

--- components/Content.tsx
+++ components/Content.tsx
@@ -1,47 +1,54 @@
-// Content.tsx
-
 import React, { useState } from "react";
 import { Grid } from "@mui/material";
 import { cardData } from "@/utils/cardData";
 import Cards from "@/components/Cards";
 import LanguageSpeechCard from "./LanguageSpeechCard";
-import { StyleSheet } from "react-native";
+import { Dimensions } from "react-native";
 
 export function Content() {
   const [selectedLanguage, setSelectedLanguage] = useState("en");
 
   return (
-    <div>
+    <div style={styles.container}>
       <LanguageSpeechCard
         selectedLanguage={selectedLanguage}
         onLanguageChange={setSelectedLanguage}
       />
-      <Grid
-        container
-        spacing={2}
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-        }}
-        style={styles.container}
-      >
-        {cardData.map((card, index) => (
-          <Grid item xs={12} sm={6} md={4} key={index}>
-            <Cards
-              title={card.title}
-              imageUrl={card.imageUrl}
-              language={selectedLanguage}
-            />
-          </Grid>
-        ))}
-      </Grid>
+      <div style={styles.scrollContainer}>
+        <Grid
+          container
+          spacing={2}
+          sx={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+          }}
+        >
+          {cardData.map((card, index) => (
+            <Grid item xs={12} sm={6} md={4} key={index}>
+              <Cards
+                title={card.title}
+                imageUrl={card.imageUrl}
+                language={selectedLanguage}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
     </div>
   );
 }
-
-const styles = StyleSheet.create({
+const styles = {
   container: {
-    overflow: "scroll",
+    display: "flex",
+    flexDirection: "column" as "column",
+    height: "100vh",
   },
-});
+  scrollContainer: {
+    flex: 1,
+    overflowY: "scroll" as "scroll",
+    padding: 20,
+  },
+};
+
+export default Content;
